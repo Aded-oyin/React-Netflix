048e57290eff2aa8e72c8107fb983631938f6163
Aded-oyin <danyodele50@gmail.com>
2024-05-17 14:59:50 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Account from "./Pages/Account";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
+
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
+
     </>
   );
 }
